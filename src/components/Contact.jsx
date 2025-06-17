d9ae2bbd2de9e5bf87a2381fc07bd9a7678a4c32
Anand0047 <anandram221003@gmail.com>
2025-06-18 00:34:35 +0530

    Your message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import Swal from 'sweetalert2'
+
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "2ebf467a-9154-44ae-8383-a1f831a506ea");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+  title: "Message Received!",
+  text: "Thanks for reaching out. I'll respond shortly!",
+  icon: "success"
+});
+
+    }
+  };
+
   return (
     <section id="contact">
       <h2 className="section-title">Contact Me</h2>
       <div className="contact-content">
-      {/* <form 
-  action="https://formsubmit.co/your-email@example.com" 
-  method="POST"
+      <form 
+  onSubmit={onSubmit}
 >
   <input type="text" name="name" placeholder="Your Name" required />
   <input type="email" name="email" placeholder="Your Email" required />
@@ -15,7 +44,7 @@ const Contact = () => {
   <input type="hidden" name="_captcha" value="false" />
   <input type="hidden" name="_next" value="https://yourdomain.com/thanks.html" />
   <button type="submit">Send Message</button>
-</form> */}
+</form>
         
         <div className="contact-info">
         <div className="social-links">
